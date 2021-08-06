afbe1958886aafa3eaed1cf503c8da1f210ddafb
unknown <facundo.girardi@gmail.com>
2021-08-06 11:13:56 -0300

    login basico

---
+++ routes/api/user.route.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var router = express.Router()
+var UserController = require('../../controllers/users.controller');
+var MailController = require('../../controllers/mail.controller');
+var Authorization = require('../../auth/authorization');
+
+
+// Authorize each API with middleware and map to the Controller Functions
+/* GET users listing. */
+router.get('/test', function(req, res, next) {
+    res.send('Llegaste a la ruta de  api/user.routes');
+  });
+router.post('/registration', UserController.createUser)
+router.post('/login/', UserController.loginUser)
+router.get('/',Authorization, UserController.getUsers)
+router.post('/userByMail', Authorization, UserController.getUsersByMail)
+router.put('/', Authorization, UserController.updateUser)
+router.delete('/:id', Authorization, UserController.removeUser)
+router.post('/sendMail',MailController.sendEmail)
+
+
+
+// Export the Router
+module.exports = router;
+
+
+
+//api/users
+//api/users/registration
+//api/users/login
---
+++ routes/utils.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET utils listing. */
+router.get('/', function(req, res, next) {
+  res.send('Utils listing');
+});
+
+module.exports = router;
