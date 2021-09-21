d3eb7aefca79d67eebc2aaea2720d8245b723762
Facundo Girardi <facundo.girardi@gmail.com>
2021-09-21 00:30:44 -0300

    cambio de tipo de dato a float

--- models/Empresa.model.js
+++ models/Empresa.model.js
@@ -1,21 +1,23 @@
-var mongoose = require('mongoose')
-var mongoosePaginate = require('mongoose-paginate')
+var mongoose = require("mongoose");
+var mongoosePaginate = require("mongoose-paginate");
+require("mongoose-double")(mongoose);
 
-
+var SchemaTypes = mongoose.Schema.Types;
 var EmpresaSchema = new mongoose.Schema({
-    nombre: String,
-    codigopago : String,
-    importe : Number,
-    descripcion : String,
-    fechaVencimiento : String,
-    estado : String,
-    cuit : String,
-    cuitEmpresa : String,
-    date : Date 
+  nombre: String,
+  codigopago: String,
+  importe: {
+    type: SchemaTypes.Double,
+  },
+  descripcion: String,
+  fechaVencimiento: String,
+  estado: String,
+  cuit: String,
+  cuitEmpresa: String,
+  date: Date,
+});
 
-})
-
-EmpresaSchema.plugin(mongoosePaginate)
-const Empresa = mongoose.model('Empresa', EmpresaSchema)
+EmpresaSchema.plugin(mongoosePaginate);
+const Empresa = mongoose.model("Empresa", EmpresaSchema);
 
 module.exports = Empresa;
--- models/User.model.js
+++ models/User.model.js
@@ -1,41 +1,43 @@
-var mongoose = require('mongoose')
-var mongoosePaginate = require('mongoose-paginate')
+var mongoose = require("mongoose");
+var mongoosePaginate = require("mongoose-paginate");
+require("mongoose-double")(mongoose);
 
-
+var SchemaTypes = mongoose.Schema.Types;
 var UserSchema = new mongoose.Schema({
-    nombre: String,
-    apellido: String,
-    tipodni: String,
-    dni: String,
-    email: String,
-    estadocuenta: Number,
-    usuario: String,
-    password: String,
-    usuariotipo: Number,
-    empresa: String,
-    nacimiento: String,
-    telefono: String,
-    cuit: String,
-    calle: String,
-    altura: String,
-    ciudad: String,
-    piso: String,
-    cbu: Number,
-    cbuCC: Number,
-    nrocuenta: Number,
-    numerocajacc : Number,
-    balancecc : Number,
-    numerocajaca : Number,
-    balanceca : Number,
-    provincia : String,
-    depto : String,
-    date: Date 
+  nombre: String,
+  apellido: String,
+  tipodni: String,
+  dni: String,
+  email: String,
+  estadocuenta: Number,
+  usuario: String,
+  password: String,
+  usuariotipo: Number,
+  empresa: String,
+  nacimiento: String,
+  telefono: String,
+  cuit: String,
+  calle: String,
+  altura: String,
+  ciudad: String,
+  piso: String,
+  cbu: Number,
+  cbuCC: Number,
+  nrocuenta: Number,
+  numerocajacc: Number,
+  balancecc: {
+    type: SchemaTypes.Double,
+  },
+  numerocajaca: Number,
+  balanceca: {
+    type: SchemaTypes.Double,
+  },
+  provincia: String,
+  depto: String,
+  date: Date,
+});
 
- 
-
-})
-
-UserSchema.plugin(mongoosePaginate)
-const User = mongoose.model('User', UserSchema)
+UserSchema.plugin(mongoosePaginate);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
